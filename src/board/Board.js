e5b4f1ef6a004bda9a3bc2ad105f3d76d274ae1a
Antonio Bautista <Antonio.Bautista84@t-mobile.com>
2020-12-24 10:32:19 -0600

    css for board and history game, go to movement x functionality

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import './Board.css';
 
-const SquareView = ({value, onClickProp }) => {
-    return(
-        <div className="square"  onClick={ () => onClickProp()}> { value }</div>
+const SquareView = ({ value, onClickProp }) => {
+    return (
+        <div className="square" onClick={() => onClickProp()}> { value}</div>
     );
 }
 export class SquareObj {
-    constructor(id = '', value = ''){
+    constructor(id = '', value = '') {
         this.id = id;
         this.value = value
     }
 }
+export const HistoryGame = ({gameLength}) => {
+    
+}
 export class Board extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             isPlayingX: true,
@@ -21,7 +24,7 @@ export class Board extends Component {
             historyGame: []
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.setState(state => {
             return {
                 ...state,
@@ -35,45 +38,78 @@ export class Board extends Component {
             arr[i] = new SquareObj(i, '*')
         }
         return arr;
-      }
-      handleClick(index){
-          // update actual board, push the actual board into the history, calculate a winner, etc
-          let { isPlayingX, boardActualGame, historyGame } = this.state;
-          // clone the array, remember that this is an array of objects, even though you clone the array
-          // the references to the objects are the same let cloned = [...arr] won't work
-          let cloneActualGame = JSON.parse(JSON.stringify(boardActualGame));
-          // concat the arrays
-          historyGame = [...historyGame, cloneActualGame];
-         console.log(historyGame);
-          cloneActualGame[index].value = isPlayingX ? 'X': 'O';
-          this.setState((state)=> {
-              return {
-                  boardActualGame: cloneActualGame,
-                  isPlayingX: !isPlayingX,
-                  historyGame: historyGame
-              }
-          });
-      }
-      componentDidUpdate(){
-          
-      }
+    }
+    handleClick(index) {
+        // update actual board, push the actual board into the history, calculate a winner, etc
+        let { isPlayingX, boardActualGame, historyGame } = this.state;
+        // clone the array, remember that this is an array of objects, even though you clone the array
+        // the references to the objects are the same let cloned = [...arr] won't work
+        let cloneActualGame = JSON.parse(JSON.stringify(boardActualGame));
+        // concat the arrays
+        historyGame = [...historyGame, cloneActualGame];
+        cloneActualGame[index].value = isPlayingX ? 'X' : 'O';
+        this.setState((state) => {
+            return {
+                boardActualGame: cloneActualGame,
+                isPlayingX: !isPlayingX,
+                historyGame: historyGame
+            }
+        }, () => {
+            this.calculateWinner();
+        });
+    }
+    calculateWinner() {
 
-      renderSquare({ id, value }){
-          return(
+    }
+    goToMovement(index){
+        let boardGameAt = this.state.historyGame[index];
+        console.log(boardGameAt);
+        this.setState(state => {
+            return {
+                ...state,
+                boardActualGame: boardGameAt
+            }
+        })
+    }
+
+    renderSquare({ id, value }) {
+        return (
             <SquareView
-            key={id}
-            value = { value }
-            onClickProp = { () => this.handleClick(id)}
+                key={id}
+                value={value}
+                onClickProp={() => this.handleClick(id)}
             />
-          )
-      }
-      render(){
-          const { boardActualGame } = this.state;
-          return <div className="board">
-              {
-                  boardActualGame.map(square => this.renderSquare(square))
-              }
-          </div>
-      }
+        )
+    }
+    renderHistory(historyGame){
+        if(historyGame && historyGame.length){
+            // iterate the array
+            return <ul>
+                {
+                    historyGame.map((val, index) => {
+                        return <li key={index} onClick={ () => this.goToMovement(index)}>Go to movement { index + 1 }</li>
+                    })
+                }
+            </ul>
+            
+        }
+    }
+    render() {
+        const { boardActualGame, historyGame } = this.state;
+        return <div className="containerTic">
+            <div className="board">
+                {
+                    boardActualGame.map(square => this.renderSquare(square))
+                }
+            </div>
+            <div className="historyGame">
+                <h4>Game movements</h4>
+                {
+                    this.renderHistory(historyGame)
+                }
+            </div>
+        </div>
+
+    }
 }
 
