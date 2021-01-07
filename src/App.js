6b73155cc7c95ff5e0ca5977666a5fc481caf9b7
Antonio Bautista <Antonio.Bautista84@t-mobile.com>
2021-01-07 10:12:21 -0600

    AuthContext, wrap App with AuthProvider and useContext in AuthGuardRoute(rendder a component based in the current user)

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Login } from './auth/login';
 import { SignUp } from './auth/signup'
 import { Board } from './board/Board';
-import app from './firebase/configuration'
+import app from './firebase/configuration';
+import { AuthProvider } from './auth/authContext';
 
 function App(props) {
   const { match, location, history } = props;
   console.log(match, location, history);
   return (
     <div className="App">
-      <header className="App-header">
-        Tic tac toe
-        <button onClick={ ()=> app.auth().signOut()}>sign out button</button>
-      </header>
-      <Router>
-        <Switch>
-          <Route exact path="/login" component = {Login}/>
-          <Route exact path="/signup" component= { SignUp }/>
-          <Route exact path="/" component= { Board }/>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <header className="App-header">
+          Tic tac toe
+        <button onClick={() => app.auth().signOut()}>sign out button</button>
+        </header>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/" component={Board} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
